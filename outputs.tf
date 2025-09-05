08f21c9a6d4e68cdd790e77beb1faddfcbc08b1b
Eric Scheidemantle <shidesthewise@zoho.com>
2025-09-05 15:25:51 -0700

    Use correct name of resource which ends with instance, not engine

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "instance_url" {
-  value = google_compute_engine.default.self_link
+  value = google_compute_instance.default.self_link
 }
 
 output "instance_id" {
-  value = google_compute_engine.default.instance_id
+  value = google_compute_instance.default.instance_id
 }
 
 output "instance_ip" {
-  value = google_compute_engine.default.network_interface.0.network_ip
+  value = google_compute_instance.default.network_interface.0.network_ip
 }
