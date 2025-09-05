1246f6618217763e788c31d9da8f9e01c387e4ef
Eric Scheidemantle <shidesthewise@zoho.com>
2025-09-05 16:38:41 -0700

    Add another vm and network to test HCP

--- main.tf
+++ main.tf
@@ -17,3 +17,23 @@ resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
   auto_create_subnetworks = "true"
 }
+
+resource "google_compute_instance" "enterprise" {
+  project      = "scheidemantlee-sandbox"
+  zone         = "us-west4-c"
+  name         = "terraform"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+  network_interface {
+    network = google_compute_network.subspace.name
+  }
+}
+
+resource "google_compute_network" "subspace" {
+  name                    = "subspace-network"
+  auto_create_subnetworks = "true"
+}
